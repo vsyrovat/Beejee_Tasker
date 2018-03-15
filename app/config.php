1a741db8d18a159c10ec78bc5d7bdd22bd730805
V S <vsyrovat@gmail.com>
2018-03-15 20:24:53 +0300

    config

--- app/config.php
+++ app/config.php
@@ -1,5 +1,4 @@
 <?php
 
-define('APP_ROOT', dirname(__DIR__));
-define('APP_CACHE_DIR', dirname(__DIR__).'/tmp/cache');
-define('TWIG_CACHE_DIR', APP_CACHE_DIR.'/twig');
+require 'config/paths.php';
+require 'config/db.php';
---
+++ app/config/paths.php
@@ -0,0 +1,5 @@
+<?php
+
+define('APP_ROOT', dirname(dirname(__DIR__)));
+define('APP_CACHE_DIR', APP_ROOT.'/tmp/cache');
+define('TWIG_CACHE_DIR', APP_CACHE_DIR.'/twig');
