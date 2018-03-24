ec303c53cca414d42b0ac276f6ac1ab18ca856c6
V S <vsyrovat@gmail.com>
2018-03-24 17:44:32 +0300

    Debug dump of pimple container

--- web/index.php
+++ web/index.php
@@ -7,3 +7,7 @@ define('DEBUG', true);
 require_once __DIR__ . '/../app/bootstrap.php';
 
 $app->run();
+
+if ($app['debug']) {
+    $app->register(new \Framework\Debug\PimpleDumpProvider(APP_ROOT));
+}
